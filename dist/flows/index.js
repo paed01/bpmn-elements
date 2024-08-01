2c1301df14439c82ddc54105fb1fcaf91c62cf0f
Pål Edman <pal.edman@gmail.com>
2024-08-01 08:57:51 +0200

    export flows

---
+++ dist/flows/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Association", {
+  enumerable: true,
+  get: function () {
+    return _Association.default;
+  }
+});
+Object.defineProperty(exports, "MessageFlow", {
+  enumerable: true,
+  get: function () {
+    return _MessageFlow.default;
+  }
+});
+Object.defineProperty(exports, "SequenceFlow", {
+  enumerable: true,
+  get: function () {
+    return _SequenceFlow.default;
+  }
+});
+var _Association = _interopRequireDefault(require("./Association.js"));
+var _MessageFlow = _interopRequireDefault(require("./MessageFlow.js"));
+var _SequenceFlow = _interopRequireDefault(require("./SequenceFlow.js"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
